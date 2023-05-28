67f8d381ee3810064145d5ba13feac9b9b2c9cda
Luis John <mnrdzjohn@gmail.com>
2023-05-28 20:29:56 +0200

    🛍️Tried Adding shop with DummyAPI

---
+++ src/app/free/laboritory/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen ml-24 mt-10 p-10'>
+        <div className='grid grid-cols-1 justify-center'>
+            <h1 className=' flex justify-center text-4xl font-extrabold'>LABS</h1>
+            <p className='flex justify-center text-lg'>single dose videos and tutorials</p>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default page
