0f711085debd807ad16266429c0e7e1e51870b8a
Luis John <mnrdzjohn@gmail.com>
2023-05-28 16:52:33 +0200

    Whole App Comit

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,23 +1,22 @@
 import getUsers from "./UsersApi";
 
 const Users = async () => {
-    const users = await getUsers();
+  const users = await getUsers();
   return (
-    
     <div>
-        {users.map((user)=>{
-            return(
-                <div>
-                    <p>{user.id}</p>
-                    <p>{user.name}</p>
-                    <p>{user.username}</p>
-                    <p className="text-2xl font-bold">{user.address.street}</p>
-                    <hr/>
-                </div>
-            ) 
-        })}
+      {users.map((user) => {
+        return (
+          <div>
+            <p>{user.id}</p>
+            <p>{user.name}</p>
+            <p>{user.username}</p>
+            <p className="text-2xl font-bold">{user.address.street}</p>
+            <hr />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
--- src/components/RegisterPop.jsx
+++ src/components/RegisterPop.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const RegisterPop = (props) => {
-
-    let modalOpen = props.modalOpen;
+  let modalOpen = props.modalOpen;
   return (
-
     <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
-        <div className="modal-box relative">
-
-            <label 
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-            onClick={()=>{props.setModalOpen(!modalOpen)}} 
-            >✕</label>
-            {props.children}
-        </div>
+      <div className="modal-box relative">
+        <label
+          className="btn btn-sm btn-circle absolute right-2 top-2"
+          onClick={() => {
+            props.setModalOpen(!modalOpen);
+          }}
+        >
+          ✕
+        </label>
+        {props.children}
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default RegisterPop
+export default RegisterPop;
